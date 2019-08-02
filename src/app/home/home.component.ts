20e080ca55ac135ca5272f14f0b9ea84592402fa
hushunding <hushunding@aliyun.com>
2019-08-02 21:33:49 +0800

    改进数据加载

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,102 +6,101 @@ import { NzTableComponent } from 'ng-zorro-antd';
 import { fstat } from 'fs';
 
 interface viewTypeInfo {
-  table: string,
-  filtes: string[]
+    table: string,
+    filtes: string[]
 }
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('nzTable', { static: false }) nzTableComponent: NzTableComponent;
-  constructor(private db: DBDataService, private es: ElectronService) { }
+    @ViewChild('nzTable', { static: false }) nzTableComponent: NzTableComponent;
+    constructor(private db: DBDataService, private es: ElectronService) { }
 
-  public DBDATA: QueryResults;
-  loading = false;
-  loaded = false;
-  tabledata: ValueType[][] = [];
-  filtes = new Map<string, ValueType[]>();
+    loading = false;
+    loaded = false;
+    tabledata: { t: string, v: ValueType }[][] = [];
+    viewdata = this.tabledata;
+    filtes = new Map<string, ValueType[]>();
 
-  pictrues: viewTypeInfo = {
-    table: 'pictrues',
-    filtes: ['name']
-  };
-  MyStor: viewTypeInfo = {
-    table: 'MyStor',
-    filtes: ['分类']
-  };
-  dbfilepath = '';
-  viewname: viewTypeInfo = null;
-  searchValue = "";
+    pictrues: viewTypeInfo = {
+        table: 'pictrues',
+        filtes: ['name']
+    };
+    MyStor: viewTypeInfo = {
+        table: 'MyStor',
+        filtes: ['分类']
+    };
+    dbfilepath = '';
+    viewname: viewTypeInfo = null;
+    searchValue = "";
 
-  opendbdata(path: string) {
-    this.viewname = path.endsWith('pic.db3') ? this.pictrues : path.endsWith('mystor.db') ? this.MyStor : null;
-    if (this.viewname === null) {
-      console.log('错误的数据文件');
-      return;
+    // 加载数据库
+    opendbdata(path: string) {
+        this.viewname = path.endsWith('pic.db3') ? this.pictrues : path.endsWith('mystor.db') ? this.MyStor : null;
+        if (this.viewname === null) {
+            console.log('错误的数据文件');
+            return;
+        }
+        this.dbfilepath = path;
+        this.loading = true;
+
+        this.db.loaddb(path, this.viewname.table, this.viewname.filtes).then(({ total, filteitems }) => {
+            this.filtes = filteitems;
+            const tabledata = total.values.map((v, i) => {
+                v.unshift(i);
+                return v.map(v2 => {
+                    const typestr = typeof v2;
+                    if (typestr === 'number' || typestr === 'string' || v2 == null) {
+                        return { t: 'raw', v: v2 }
+                    }
+                    else {
+                        const img = this.es.nativeImage.createFromBuffer(Buffer.from((v2 as Uint8Array).buffer));
+                        return { t: 'img', v: img.toDataURL() };
+                    }
+                })
+            });
+
+            this.tabledata = tabledata;
+            this.viewdata = this.tabledata;
+            this.loading = false;
+            this.loaded = true;
+        });
     }
-    this.dbfilepath = path;
-    this.loading = true;
-    this.db.loaddb(path, this.viewname.table, this.viewname.filtes).then(({ total, filteitems }) => {
-      this.DBDATA = total;
-      this.filtes = filteitems;
 
-      const tabledata = total.values as any;
-      for (let i = 0; i < tabledata.length; i++) {
-        tabledata[i].unshift(i);
-      }
-      this.tabledata = tabledata;
-      this.loading = false;
-      this.loaded = true;
-    });
-  }
-  // MyStor
-  ngOnInit() {
 
-  }
-  isTextData(i: ValueType) {
-    const typestr = typeof i;
-    return typestr === 'number' || typestr === 'string';
-  }
+    ngOnInit() {
 
-  toImage(item: Uint8Array, i: number, j: number) {
-    if (item !== null) {
-      const img = this.es.nativeImage.createFromBuffer(Buffer.from(item.buffer));
-      return (img.toDataURL());
-    } else {
-      return ('');
     }
-  }
-  scrollToIndex(index: number): void {
-    // this.nzTableComponent.cdkVirtualScrollViewport.scrollToIndex(index);
-  }
+    scrollToIndex(index: number): void {
+        // this.nzTableComponent.cdkVirtualScrollViewport.scrollToIndex(index);
+    }
 
-  ngAfterViewInit(): void {
-    // this.nzTableComponent.cdkVirtualScrollViewport.scrolledIndexChange
-    //   .subscribe((data: number) => {
-    //     console.log('scroll index to', data);
-    //   });
-  }
-  trackByIndex(_: number, data): number {
-    return data[0];
-  }
-  // 获取DB文件
-  SelectDBFile() {
-    this.es.remote.dialog.showOpenDialog({
-      title: '选择数据文件',
-      filters: [{ name: 'db文件', extensions: ['db', 'db3'] }]
-    }).then(result => {
-      if (result.filePaths.length !== 0) {
-        this.opendbdata(result.filePaths[0]);
-      }
-    }).catch(err => {
-      console.log(err);
-    });
+    ngAfterViewInit(): void {
+        // this.nzTableComponent.cdkVirtualScrollViewport.scrolledIndexChange
+        //   .subscribe((data: number) => {
+        //     console.log('scroll index to', data);
+        //   });
+    }
+    trackByIndex(_: number, data): number {
+        return data[0];
+    }
+    // 获取DB文件
+    SelectDBFile() {
+        this.es.remote.dialog.showOpenDialog({
+            title: '选择数据文件',
+            filters: [{ name: 'db文件', extensions: ['db', 'db3'] }]
+        }).then(result => {
+            if (result.filePaths.length !== 0) {
+                this.opendbdata(result.filePaths[0]);
+            }
+        }).catch(err => {
+            console.log(err);
+        });
 
-  }
+    }
 
 }
